5ad1e2c6e3404123b0125e4fffd7e27fa3caa6cb
Eric Khangati <ekhangati@gmail.com>
2024-02-26 16:11:58 +0300

    Register URLs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -8,6 +8,8 @@ app = Flask(__name__)
 # mpesa details
 MPESA_KEY = os.environ['MPESA_KEY']
 MPESA_SECRET = os.environ['MPESA_SECRET']
+SHORTCODE = os.environ['SHORTCODE']
+BASE_URL = os.environ['BASE_URL']
 
 
 @app.route("/")
@@ -31,6 +33,47 @@ def ac_token():
         return data
     else:
         return {"error": "Failed to get access token"}, 500
+
+
+# register urls
+@app.route("/register_urls")
+def register():
+    mpesa_endpoint = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+    headers = {"Authorization": f"Bearer {ac_token()}"}
+    req_body = {
+        "ShortCode": SHORTCODE,
+        "ResponseType": "Completed",
+        "ConfirmationURL": BASE_URL + "/c2b/confirm",
+        "ValidationURL": BASE_URL + "/c2b/validation"
+    }
+
+    response_data = requests.post(
+        url=mpesa_endpoint,
+        json=req_body,
+        headers=headers
+    )
+
+    return response_data.json()
+
+
+@app.route("/c2b/confirm")
+def confirm():
+    # get data
+    data = request.json
+
+    # write to file
+    with open("confirm.json", "a") as file:
+        file.write(data)
+
+
+@app.route("/c2b/validation")
+def validation():
+    # get data
+    data = request.json
+
+    # write to file
+    with open("confirm.json", "a") as file:
+        file.write(data)
 
 
 if __name__ == "__main__":
