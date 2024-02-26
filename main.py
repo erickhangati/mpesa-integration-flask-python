0b0a1a7b8a1acdc4600f1bf6532a1086f26f3dfa
Eric Khangati <ekhangati@gmail.com>
2024-02-26 13:17:29 +0300

    Access token

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+import os
 from flask import Flask
+import requests
+from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
+
+# mpesa details
+MPESA_KEY = os.environ['MPESA_KEY']
+MPESA_SECRET = os.environ['MPESA_SECRET']
 
 
 @app.route("/")
 def home():
     return "Hello World!"
+
+
+# access token
+@app.route("/access_token")
+def token():
+    data = ac_token()
+    return data
+
+
+def ac_token():
+    mpesa_auth_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+    response = requests.get(mpesa_auth_url, auth=HTTPBasicAuth(MPESA_KEY, MPESA_SECRET))
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return {"error": "Failed to get access token"}, 500
 
 
 if __name__ == "__main__":
